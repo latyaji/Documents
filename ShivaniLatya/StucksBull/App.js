25c4df47bb2937b79b6a6410d33b9dd6acfdb6d1
latyaji <shivani.latya@taskcraft.in>
2023-09-05 10:51:02 +0530

    initial commit

---
+++ ShivaniLatya/StucksBull/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Shivani latay</Text>
+      <Text>My name is  hshs sdfghjk frgthjkgfdfghj rtyuioiuyt</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
